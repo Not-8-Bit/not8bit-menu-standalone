3fb559716b4f74744ed7c594d52ae3fefb3cd6aa
xholiday81x@gmail.com <xholiday81x@gmail.com>
2023-12-26 14:37:48 -0600

    manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,19 @@
 name 'Holiday Menu System'
 author 'Not 8 Bit - Holiday'
-version 'v1.0.0'
+version 'v1.0.1'
 
 description 'Menu system with unlimited text rows amd image capability for buttons and header.'
 
 fx_version 'cerulean'
 games { 'gta5' }
 
-client_script 'client/main.lua'
+client_scripts {
+    'client/*.lua' 
+}
+
+server_scripts {
+    'server/*.lua'
+}
 
 ui_page 'html/index.html'
 
